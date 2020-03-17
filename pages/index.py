71d931b96cb87522dc88395b1088d96871a41434
Harsh Desai <hurshd0@gmail.com>
2020-03-17 07:37:28 +0000

    app: added scatter geomapbox showing covid19 cases

--- pages/index.py
+++ pages/index.py
@@ -13,6 +13,8 @@ import json
 from utils.utils import CovidMongo
 from app import app
 import plotly.express as px
+import plotly.graph_objects as go
+import dash_daq as daq
 
 ########################################################################
 #
@@ -20,53 +22,10 @@ import plotly.express as px
 #
 #########################################################################
 
-# TODO: Get Data from MongoDB
-
-cm = CovidMongo("covid", "state", verbose=False)
-
 DEFAULT_OPACITY = 0.8
-
+cm = CovidMongo("covid", "state", verbose=False)
 mapbox_access_token = config("MAPBOX_ACCESS_TOKEN")
-mapbox_style = "mapbox://styles/plotlymapbox/cjvprkf3t1kns1cqjxuxmwixz"
-
-########################################################################
-#
-# App layout
-#
-########################################################################
-
-df = cm.get_records_in_df()
-df["Latitude"]
-# df = px.data.carshare()
 px.set_mapbox_access_token(mapbox_access_token)
-
-fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude",     color="Confirmed", size="Confirmed",
-                        color_continuous_scale=px.colors.cyclical.IceFire)
-
-layout = html.Div(
-    [
-        dbc.Row(
-            [
-                dbc.Col(html.Div("Total Confirmed")),
-                dbc.Col(html.Div("Total Deaths")),
-                dbc.Col(html.Div("Total Recovered")),
-            ]
-        ),
-        dbc.Row(
-            [
-                dbc.Col(html.Div("Line Chart")),
-                dbc.Col([
-                    # dcc.Graph(id='us-map'),
-                    dcc.Graph(figure=fig)
-                ]
-                ),
-                dbc.Col(html.Div("Twitter Feed")),
-            ]
-        ),
-    ]
-)
-
-
 ########################################################################
 #
 # App Callbacks
@@ -74,33 +33,136 @@ layout = html.Div(
 ########################################################################
 
 
-def total_top_bar():
+def build_scatter_mapbox():
+    df = cm.get_records_in_df()
+    # fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="Confirmed", size="Confirmed", hover_name="Province/State", hover_data={"Confirmed", "Deaths", "Recovered"},
+    #                         color_continuous_scale=px.colors.cyclical.IceFire)
+    # # fig = go.Figure(go.Scattermapbox(lat=df.Latitute,
+    # #                                  lon=df.Longtitude,
+    # #                                  mode='markers',
+    # # 39.8097343, -98.5556199
+
+    data = go.Scattermapbox(
+        lat=df["Latitude"],
+        lon=df["Longitude"],
+        mode="markers",
+        marker=go.scattermapbox.Marker(
+            size=14
+        ),
+        # text={"Confirmed": df["Confirmed"],
+        #       "Deaths": df["Deaths"], "Recovered": df["Recovered"]},
+        # hoverinfo='text'
+
+    )
+
+    layout = go.Layout(
+        autosize=True,
+        hovermode='closest',
+        mapbox=go.layout.Mapbox(
+            bearing=0,
+            center=go.layout.mapbox.Center(lat=0, lon=0),
+            pitch=0,
+            zoom=3.5
+        ),
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},
+                      mapbox_style="dark",
+                      mapbox=dict(accesstoken=mapbox_access_token,
+                                  center=dict(lat=39.8097343,
+                                              lon=-98.5556199),
+                                  zoom=3.5))
+    return fig
+
+
+def build_top_bar():
     """Total tested cases, confirmed cases, death cases card at the top row.
+
+    :params: none
+    :returns: tested: number of tested cases; confirmed: number of confirmed cases; deaths: number of deaths.
     """
+    try:
+        response = requests.get(
+            url="https://covidtracking.com/api/us").json()[0]
+        tested = response['posNeg']
+        confirmed = response['positive']
+        deaths = response['death']
+    # df=cm.get_records_in_df()
+    # deaths=df[""]
+    # recovered=df["Recovered"]
+    except:
+        confirmed = 0
+        deaths = 0
+        tested = 0
 
-    response = requests.get(url="https://covidtracking.com/api/us").json()
+    cols = [
+        dbc.Col(html.Div(
+            id="card-1",
+            children=[
+                html.P("Total Tested"),
+                daq.LEDDisplay(
+                    id="total-confirmed-led",
+                    value=tested,
+                    color="#92e0d3",
+                    backgroundColor="#1e2130",
+                    size=50,
+                ),
+            ],
+        ), md=4),
+        dbc.Col(
+            html.Div(
+                id="card-2",
+                children=[
+                    html.P("Total Confirmed"),
+                    daq.LEDDisplay(
+                        id="total-confirmed-led",
+                        value=confirmed,
+                        color="#92e0d3",
+                        backgroundColor="#1e2130",
+                        size=50,
+                    ),
+                ],
+            ),
+            md=4
+        ),
+        dbc.Col(html.Div(
+            id="card-3",
+            children=[
+                html.P("Total Deaths"),
+                daq.LEDDisplay(
+                    id="total-confirmed-led",
+                    value=deaths,
+                    color="#92e0d3",
+                    backgroundColor="#1e2130",
+                    size=50,
+                ),
+            ],
+        ), md=4),
+    ]
 
-    raise NotImplementedError
+    return cols
+
+# @app.callback(Output("us-map", "figure"), [Input("map-input", "value")])
 
 
-# @app.callback(Output('us-map', 'figure'))
-# def scatter_mapbox(state=None):
-#     """Displays choroplepth map for the data. For the whole US, the map is divided by state. For individual states,
-#     the map will be divided by county lines.
+def scatter_mapbox(state=None):
+    """Displays choroplepth map for the data. For the whole US, the map is divided by state. For individual states,
+    the map will be divided by county lines.
 
-#     :param state: get the time series data for a particular state for confirmed, deaths, and receovered. If None, the whole US.
-#     """
-#     if state is not None:
-#         state_data = cm.get_data_by_state(state)
-#     else:                       # US
-#         us_data = cm.get_records_in_df()
+    :param state: get the time series data for a particular state for confirmed, deaths, and receovered. If None, the whole US.
+    """
+    if state is not None:
+        df = cm.get_data_by_state(state)
+    else:                       # US
+        df = cm.get_records_in_df()
 
-#     # df = px.data.carshare()
-#     px.set_mapbox_access_token(mapbox_access_token)
-#     fig = px.scatter_mapbox(us_data, lat="Latitute", lon="Longitute",  color="Deaths", size="Deaths",
-#                             color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
+    df = cm.get_records_in_df()
 
-#     return fig
+    fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="Confirmed", size="Confirmed", hover_name="Province/State", hover_data={"Confirmed", "Deaths", "Recovered"},
+                            color_continuous_scale=px.colors.cyclical.IceFire, zoom=3)
+
+    return fig
 
 
 def bar_chart_left(state=None):
@@ -137,3 +199,44 @@ def twitter_feed_right(state=None):
     """
 
     raise NotImplementedError
+
+
+########################################################################
+#
+# App layout
+#
+########################################################################
+
+# df = cm.get_records_in_df()
+# # df = px.data.carshare()
+# px.set_mapbox_access_token(mapbox_access_token)
+# fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="Confirmed", size="Confirmed", hover_name="Province/State", hover_data={"Confirmed", "Deaths", "Recovered"},
+#                         color_continuous_scale=px.colors.cyclical.IceFire, zoom=3)
+# fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+# df = px.data.carshare()
+# fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon",     color="peak_hour", size="car_hours",
+#                         color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
+# fig.show()
+
+layout = html.Div(
+    [
+        dbc.Row(
+
+            build_top_bar()
+
+        ),
+        dbc.Row(
+            [
+
+                dbc.Col([
+                    # dcc.Input(id='map-input', value=None),
+                    dcc.Graph(id='us-map', figure=build_scatter_mapbox()),
+                ]
+                )
+
+            ]
+        ),
+    ]
+
+
+)
