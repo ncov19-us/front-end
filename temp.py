b1f1474d1fbfedacd06756edbf3879a8114fc3ab
Harsh Desai <hurshd0@gmail.com>
2020-03-17 07:35:50 +0000

    chore: trying out things in temp

--- temp.py
+++ temp.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from utils.utils import CovidMongo
 import json
+import requests
 
 cm = CovidMongo("covid", "state", verbose=False)
 
@@ -8,5 +9,11 @@ cm = CovidMongo("covid", "state", verbose=False)
 
 # print(state.get_data_by_state("TX"))
 
+# df = cm.get_records_in_df()
+# print(df.info())
+# print(df.head())
+# print(df["Recovered"].sum())
 
-print(cm.get_records_in_df())
+
+response = requests.get(url="https://covidtracking.com/api/us").json()
+print(response)
