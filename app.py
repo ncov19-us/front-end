2f90e7d9d02f6672366b7c2ed65b06457ca2c55a
leehanchung <kein520@yahoo.com>
2020-03-18 18:11:44 -0700

    han: hursh: added google analytics

--- app.py
+++ app.py
@@ -25,6 +25,36 @@ app = dash.Dash(
     external_stylesheets=external_stylesheets,
     meta_tags=meta_tags)
 
+
+app.index_string = """<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T7EJZN12YD"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-T7EJZN12YD');
+        </script>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>"""
+
+
+
 app.config.suppress_callback_exceptions = True
 app.title = 'COVID19 US Dashboard'  # for browser titlebar
 server = app.server
