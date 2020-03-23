4b9778aad99b5d897648b2655ca562699dcd45c3
Elizabeth Ter Sahakyan <liztersahakyan@gmail.com>
2020-03-23 01:57:33 -0700

    added mobile css classes

--- pages/mobile_footer.py
+++ pages/mobile_footer.py
@@ -14,29 +14,29 @@ mobile_footer = dbc.Container(
                         and not limited to U.S. Federal, State, and local governments, WHO,
                         and John Hopkins CSSE. News feeds obtained from Twitter and NewsAPI. The content of
                         this Website is for information purposes and makes no guarantee to be accurate.""",
-                    className="footer-disclaimer-text",
+                    className="mobile-footer-disclaimer-text",
                 ),
-                className="footer-disclaimer-content",
+                className="mobile-footer-disclaimer-content",
                 width=10,
             ),
             dbc.Col(
                 [
                     html.Span(
                         html.A(
-                            html.I(className="fab fa-github"),
+                            html.I(className="mobile-fab fa-github"),
                             href="https://github.com/ncov19-us/front-end",
                         ),
-                        className="footer-social-icons mr-3",
+                        className="mobile-footer-social-icons mr-3",
                     ),
                     html.Span(
-                        "Copyright ncov19.us 2020", className="footer-copyright-text"
+                        "Copyright ncov19.us 2020", className="mobile-footer-copyright-text"
                     ),
                 ],
-                className="footer-social-copyright-content",
+                className="mobile-footer-social-copyright-content",
                 width=2,
             ),
         ],
     ),
     fluid=True,
-    className="footer-content",
+    className="mobile-footer-content",
 )
