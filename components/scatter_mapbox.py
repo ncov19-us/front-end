04f390cbc0bda014277c01e8f1b1a0a0c5040603
Elizabeth Ter Sahakyan <liztersahakyan@gmail.com>
2020-03-24 23:31:28 -0700

    liz:daniel: merge css styling

--- components/scatter_mapbox.py
+++ components/scatter_mapbox.py
@@ -158,7 +158,7 @@ def drive_thru_scatter_mapbox():
     )
     fig.layout.update(
         margin={"r": 0, "t": 0, "l": 0, "b": 0},
-        mapbox_style="white-bg",
+        mapbox_style="dark",
         mapbox=dict(center=dict(lat=39.8097343, lon=-98.5556199), zoom=2.3),
         dragmode=False,
     )
