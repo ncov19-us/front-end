3bc97d3024e5e79a2d7542ec5d72102e1a835d9d
leehanchung <kein520@yahoo.com>
2020-03-29 15:35:42 -0700

    han: app: moved backend to production api from staging api

--- utils/settings.py
+++ utils/settings.py
@@ -8,7 +8,7 @@ MAPBOX_ACCESS_TOKEN = config("MAPBOX_ACCESS_TOKEN")
 MAPBOX_STYLE = "mapbox://styles/hurshd0/ck86zky880ory1ip18f5tw4y6"
 
 # ncov19.us API
-NCOV19_API = "https://covid19-us-api-staging.herokuapp.com/"
+NCOV19_API = "https://covid19-us-api.herokuapp.com/"
 
 # Drive Thru Facilities
 DRIVE_THRU_URL = "https://raw.githubusercontent.com/ncov19-us/ds/master/drive_thru_testing_locations/us-drive-thru-testing-locations.csv"
