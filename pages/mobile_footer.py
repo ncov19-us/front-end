baa2eebfbd916e5a3fb0a44ccd1289c9957d59f0
Elizabeth Ter Sahakyan <liztersahakyan@gmail.com>
2020-03-29 11:58:23 -0700

    liz: changes to footer copyright text and width

--- pages/mobile_footer.py
+++ pages/mobile_footer.py
@@ -12,8 +12,7 @@ mobile_footer = dbc.Row(
                         and not limited to U.S. Federal, State, and local governments, WHO,
                         and John Hopkins CSSE. News feeds obtained from Twitter and NewsAPI. The contents of
                         this Website are for information purposes only and are not guaranteed to be accurate.  
-                        Reliance on this Website for medical guidance or use of the Website in commerce is 
-                        strictly prohibited.""",
+                        Reliance on this Website for medical guidance is strictly prohibited.""",
                     className="mobile-footer-disclaimer-text",
                 ),
                 className="mobile-footer-disclaimer-content"  
