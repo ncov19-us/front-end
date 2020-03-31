68abeda8dd10435347feabbabd587afa57ef2e66
Harsh Desai <hurshd0@gmail.com>
2020-03-31 10:29:40 -0500

    Update settings.py

--- utils/settings.py
+++ utils/settings.py
@@ -8,8 +8,8 @@ MAPBOX_ACCESS_TOKEN = config("MAPBOX_ACCESS_TOKEN")
 MAPBOX_STYLE = "mapbox://styles/hurshd0/ck86zky880ory1ip18f5tw4y6"
 
 # ncov19.us API
-# NCOV19_API = "https://covid19-us-api.herokuapp.com/"
-NCOV19_API = "https://8d9aa1ed.ngrok.io/"
+NCOV19_API = "https://covid19-us-api.herokuapp.com/"
+#NCOV19_API = "https://8d9aa1ed.ngrok.io/"
 
 # Drive Thru Facilities
 DRIVE_THRU_URL = "https://raw.githubusercontent.com/ncov19-us/ds/master/drive_thru_testing_locations/us-drive-thru-testing-locations.csv"
